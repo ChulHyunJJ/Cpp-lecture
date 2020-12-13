602497ce277a94351b5d78321842b46ac6f3d241
조철현 <chris@jocheolhyeon-ui-MacBookAir.local>
2020-12-13 17:53:47 +0900

    del: Files

--- Structure/cmake-build-debug/CMakeFiles/Structure_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/Structure_autogen"
-  "Structure_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Structure_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
